0c1e16c72b159d4eee98fe9a65cc35c3d03fc84e
Mauricio Sevilla Britto <msevillab@gmail.com>
2022-06-21 18:18:57 -0500

    se actualiza base de datos

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-$database = new mysqli("localhost", "root", "", "citas");
+$database = new mysqli("localhost", "root", "", "sistema-citas");
 if ($database->connect_error) {
     die("Connection failed:  " . $database->connect_error);
 }
